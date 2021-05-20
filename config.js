c74441fe6bed035f5d0f7331b529b9a6576591b9
Nallendhiran <70853166+Nallendhiran@users.noreply.github.com>
2021-05-20 12:29:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports= {
-    apiKey: "AIzaSyCslPsmNtlSPsOF0XNUO8emZWPk4-9w9z8",
+    apiKey: "AIzaSyCslPsmNtlSPsOFO8emZWPk4-9w9z8",
     authDomain: "webapp-8f056.firebaseapp.com",
     databaseURL: "https://webapp-8f056.firebaseio.com",
-    projectId: "webapp-8f056",
+    projectId: "webapp-8f046",
     storageBucket: "webapp-8f056.appspot.com",
-    messagingSenderId: "998086983741",
-    appId: "1:998086983741:web:6d9a94750d91c488356a54",
-    measurementId: "G-9F8GEKLSP5"
+    messagingSenderId: "998083741",
+    appId: "1:998086983741:web:6d9a94791c488356a54",
+    measurementId: "G-9F8GKLSP5"
   };
